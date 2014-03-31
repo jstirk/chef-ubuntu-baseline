f7c9e6a8e767fdbd88349947f1552bd19c306826
Jason Stirk <jstirk@oobleyboo.com>
2014-03-31 19:35:10 +1100

    Bump version number for bugfix commits

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jason@achernarsolutions.com.au'
 license          'All rights reserved'
 description      'Basic setup and security hardening for an Ubuntu server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 depends 'user', '~> 0.3.0'
 depends 'openssh', '~> 1.1.4'
